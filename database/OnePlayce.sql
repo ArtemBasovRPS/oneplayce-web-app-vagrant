950988a467ca0bf0b3007cd06033869d5fa956cb
Sergey Kuzmich <SergeyKuzmich@users.noreply.github.com>
2015-12-15 13:57:10 +0300

    Rename OnePlayce.sql to oneplayce.sql

--- database/OnePlayce.sql
+++
@@ -1,2 +0,0 @@
-CREATE DATABASE IF NOT EXISTS oneplayce;
-GRANT ALL PRIVILEGES ON `oneplayce`.* TO `oneplayceu`@`%` IDENTIFIED BY 'oneplaycep';
---
+++ database/oneplayce.sql
@@ -0,0 +1,2 @@
+CREATE DATABASE IF NOT EXISTS oneplayce;
+GRANT ALL PRIVILEGES ON `oneplayce`.* TO `oneplayceu`@`%` IDENTIFIED BY 'oneplaycep';
