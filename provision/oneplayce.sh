01c856685c08b3171f9c4c82e0ecd90314165889
Sergey Kuzmich <skuzmich@remedypointsolutions.com>
2017-06-07 11:41:17 +0300

    Revert "Update development environment configuration"
    This reverts commit 904a72957fb2923a40502c471e02449e97795377.

--- provision/oneplayce.sh
+++ provision/oneplayce.sh
@@ -3,7 +3,7 @@
 # Setup REQUIRED SOFTWARE for OnePlayce
 sudo add-apt-repository -y ppa:kirillshkrogalev/ffmpeg-next
 sudo apt-get update
-sudo apt-get install -y --force-yes ffmpeg
+sudo apt-get install -y ffmpeg
 
 # Copy OnePlayce VirtualHost
 sudo cp -fRv /vagrant/conf/sites-available/oneplayce.conf /etc/apache2/sites-available/
@@ -18,7 +18,7 @@ sudo service apache2 reload
 
 # Install composer packages
 sudo composer self-update
-sudo composer global require "fxp/composer-asset-plugin" --no-progress
+sudo composer global require "fxp/composer-asset-plugin:~1.1.1" --no-progress
 sudo composer install --prefer-dist --no-progress --no-interaction --working-dir /var/sites/oneplayce
 
 # Setup OnePlayce database
